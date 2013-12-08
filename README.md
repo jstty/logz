5cd0e76195193bb489abca43db7a2849df2d429a
Joseph Sutton <joe@jstty.com>
2013-12-08 03:03:17 -0800

    Fixed node trace Fixed bump return output handler for “%” Added format function for buffer and normal output Added custom functions to basic browser example Removed some old code in lib Refactored the lib code a bit

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 
 # [![Build Status](https://secure.travis-ci.org/jstty/zjs-logz.png)](http://travis-ci.org/jstty/zjs-logz)
-
 # [![NPM](https://nodei.co/npm/logz.png)](https://nodei.co/npm/logz/)
 
 TODO:
-  - Fix node trace compatibility
-  - Fix dump return output to handle "%d" in first string
-  - Add formatFunc to display
-  - Replace (showTime,timeFormatFunc,showTrace) with formatFunc
+  - Add replaceConsole feature
+  - some traces missing
   - Add Unit(mocha) tests
   - Add karma
   - Make README.md pretty
+    - Write up all feature list
 
 Have a Nice Day!
--- examples/node/basic.js
+++ examples/node/basic.js
@@ -1,23 +1,18 @@
 var moment = require('moment');
 var LogZ   = require('../../lib/logz.js');
 
-/*
-var logz = LogZ({
-	 display: true
-	,buffer: {
-		size: 8,
-		showTime: true,
-		timeFormatFunc: function(timeDate) {
-			return moment(timeDate).format('h:mm:ss a');
-		},
-		showTrace: true
-	}
+var logz = LogZ("basic", {
+    env: "dev",
+    showTrace: true,
+    buffer: {
+        size: 8,
+        showTrace: true
+    }
 });
-*/
 
-var logz = LogZ("dev");
-
+console.log("----------------------------------------------------------");
 console.log("-- Log Output --");
+console.log("----------------------------------------------------------");
 logz.log("test");
 logz.info("test: %s", "string");
 logz.warn("test: %d", 123);
@@ -25,9 +20,9 @@ logz.error("test error: %d", 123);
 
 logz.log("before group");
 
-logz.group("test");
-    logz.log("test", "Group", 1);
-    logz.warn("test", { obj: "Group1" } );
+logz.group();
+    logz.log("test log", "Group", 1);
+    logz.warn("test warn", { obj: "Group1" } );
 
     logz.group("test 2");
         logz.log("test", "Group", 2);
@@ -38,8 +33,12 @@ logz.groupEnd();
 
 logz.log( { after: "group", a: [1,2,3,4,5,6,7,8,9,0] } );
 
+console.log("----------------------------------------------------------");
 console.log("-- Dumping Buffered Output --");
+console.log("----------------------------------------------------------");
 var dump = logz.dump();
 
+console.log("----------------------------------------------------------");
 console.log("-- Printing Buffered Output --");
+console.log("----------------------------------------------------------");
 console.log( dump.join("\n") );
